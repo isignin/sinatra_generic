5b8fa875b78042d6bc184f28224f572b73f4a11c
Steve Loo <isignin@gmail.com>
2015-07-30 21:57:37 -0400

    Update configuration in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,15 @@
 # spec/spec_helper.rb
+
+ENV['RACK_ENV'] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
  
 require 'rack/test'
 require 'rspec'
 
-ENV['RACK_ENV'] = 'test'
-
 module RSpecMixin
   include Rack::Test::Methods
-  def app() described_class end
+  def app() App end
 end
 
 RSpec.configure { |c| c.include RSpecMixin }
- 
-# set test environment
-# set :environment, :test
-# set :run, false
-# set :raise_errors, true
-# set :logging, false
+
