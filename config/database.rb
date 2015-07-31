79f14c7665f666dab2154eb58d3040d602a0d820
Steve Loo <isignin@gmail.com>
2015-07-30 21:46:28 -0400

    Select DB based on environment

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,11 @@
 require 'sinatra/sequel'
 require 'sqlite3'
- 
-DB=Sequel.sqlite("tmp/generic.db")
+
+DB = case ENV['RACK_ENV']
+  when 'test' then Sequel.sqlite
+  when 'production' then Sequel.sqlite("tmp/generic_production.db") 
+  else Sequel.sqlite("tmp/generic.db")  # development
+end
 
 unless DB.table_exists?(:users)
   DB.create_table :users do
