10c8bbead21a299258171a4fc05a2f9d52ca0845
sloo <sloo@rti.org>
2015-08-03 09:12:55 -0400

    Add users_helpers methods

--- app/helpers/users_helpers.rb
+++ app/helpers/users_helpers.rb
@@ -2,6 +2,11 @@ module MyApp
   module Helpers
     def fullname(user)
       user.first_name+" "+user.last_name
-    end  
+    end
+    
+    def yes_no(col)
+      col == 1 ? "Yes" : "No"
+    end 
+    
   end
 end  
